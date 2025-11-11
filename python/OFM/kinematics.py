3866d649acf77bef8e3cca2e05474a4bf1df2e52
oussema-dev <oussama.jlassi@hotmail.fr>
2025-11-11 09:52:58 -0500

    merge grood_suntay functions into one

--- python/OFM/kinematics.py
+++ python/OFM/kinematics.py
@@ -5,21 +5,16 @@ from utils.utils import addchannelsgs, getDir
 
 def kinematics(data, r, jnt, version):
     """ wrapper function to access different computations"""
-
-    # choose version
-    if version == "1.0":
-        KIN = grood_suntay_1_0(r, jnt)
-    elif version == "1.1":
-        KIN = grood_suntay(r, jnt)
-
+    KIN = grood_suntay(r, jnt, version)
     # update reference system
     data, _ = refsystem(data, KIN, version)
-
     return data
 
-
-def grood_suntay_1_0(r, jnt):
-
+def grood_suntay(r, jnt, version):
+    """
+    Merged version of grood_suntay_1_0 and grood_suntay.
+    Selects the correct logic based on the 'version' string.
+    """
     KIN = {}
     for i in range(len(jnt)):
         jnt_name = jnt[i]
@@ -29,101 +24,90 @@ def grood_suntay_1_0(r, jnt):
         pax = r[pbone]['ort']  # stores xyz local axes for each frame
         dax = r[dbone]['ort']
 
-        if pbone.startswith('Right'):
-            bone = pbone[5:]
-        elif pbone.startswith('Left'):
-            bone = pbone[4:]
-        else:
-            bone = pbone
-
-        # get axes
-
-        flx_j, abd_j, tw_j = None, None, None
-        if bone == 'Global':
-            floatax, _, prox_x, dist_x, prox_y, dist_y, prox_z, dist_z = makeax(pax, dax)
-            # computing with respect to i and j directions requires different axes
-            flx_i = angle(dist_z, prox_x)
-            abd_i = angle(prox_y, dist_z)
-            tw_i = angle(dist_x, prox_y)
-
-            flx_j = angle(prox_y, dist_z)
-            abd_j = angle(dist_z, prox_x)
-            tw_j = angle(dist_x, prox_x)
-
-        if bone == 'TibiaOFM':
-            floatax, prox_x, dist_x, prox_y, dist_y, prox_z, dist_z = makeax_1_0(pax, dax)
-            # computing tibia/hindfoot and tibia/forefoot angles
-            alpha = -angle(floatax, prox_x)
-            beta = angle(prox_y, dist_z)
-            gamma = angle(floatax, dist_y)
-
-        else:
-            floatax, prox_x, dist_x, prox_y, dist_y, prox_z, dist_z = makeax_1_0(pax, dax)
-            # computing hindfoot/forefoot and forefoot/hallux angles
-            alpha = -angle(floatax, prox_z)
-            beta = angle(prox_y, dist_z)
-            gamma = angle(floatax, dist_y)
-
-        KIN[jnt_name[0]] = dict
-        if bone == 'Global':
-            KIN[jnt_name[0]] = {'flx_i': flx_i, 'abd_i': abd_i, 'tw_i': tw_i,
-                                'flx_j': flx_j, 'abd_j': abd_j, 'tw_j': tw_j}
-        elif bone == 'TibiaOFM':
-            KIN[jnt_name[0]] = {'flx': alpha, 'abd': gamma, 'tw': beta}
-        else:
-            KIN[jnt_name[0]] = {'flx': alpha, 'abd': beta, 'tw': gamma}
-
-    return KIN
-
-
-def grood_suntay(r, jnt):
-
-    KIN = {}
-    for i in range(len(jnt)):
-        jnt_name = jnt[i]
-        pbone = jnt_name[1]
-        dbone = jnt_name[2]
-
-        pax = r[pbone]['ort']  # stores xyz local axes for each frame
-        dax = r[dbone]['ort']
-
+        # Standardize variable name to 'prox_bone'
         if pbone.startswith('Right'):
             prox_bone = pbone[5:]
         elif pbone.startswith('Left'):
             prox_bone = pbone[4:]
         else:
             prox_bone = pbone
-
-        # get axes
-        floatax, _, prox_x, dist_x, prox_y, dist_y, prox_z, dist_z = makeax(pax, dax)
+        
+        # Initialize all potential angle variables
+        flx_i, abd_i, tw_i = None, None, None
         flx_j, abd_j, tw_j = None, None, None
-        if prox_bone == 'Global':
-            # computing with respect to i and j directions requires different axes
-            flx_i = angle(dist_y, prox_x)
-            abd_i = angle(prox_y, dist_y)
-            tw_i = angle(dist_x, prox_y)
+        alpha, beta, gamma = None, None, None
 
-            flx_j = angle(prox_y, dist_y)
-            abd_j = angle(dist_y, prox_x)
-            tw_j = angle(dist_x, prox_x)
+        if version == "1.0":
+            if prox_bone == 'Global':
+                floatax, _, prox_x, dist_x, prox_y, dist_y, prox_z, dist_z = makeax(pax, dax)
+                # v1.0 Global calculations
+                flx_i = angle(dist_z, prox_x)
+                abd_i = angle(prox_y, dist_z)
+                tw_i = angle(dist_x, prox_y)
+                flx_j = angle(prox_y, dist_z)
+                abd_j = angle(dist_z, prox_x)
+                tw_j = angle(dist_x, prox_x)
+            
+            elif prox_bone == 'TibiaOFM':
+                floatax, prox_x, dist_x, prox_y, dist_y, prox_z, dist_z = makeax_1_0(pax, dax)
+                # v1.0 TibiaOFM calculations
+                alpha = -angle(floatax, prox_x)
+                beta = angle(prox_y, dist_z)
+                gamma = angle(floatax, dist_y)
+            
+            else:
+                floatax, prox_x, dist_x, prox_y, dist_y, prox_z, dist_z = makeax_1_0(pax, dax)
+                # v1.0 Else calculations
+                alpha = -angle(floatax, prox_z)
+                beta = angle(prox_y, dist_z)
+                gamma = angle(floatax, dist_y)
 
-        if prox_bone == 'ForeFoot':
-            alpha = angle(floatax, prox_y)
-            beta = -angle(prox_z, dist_x)
-            gamma = angle(floatax, dist_z)
+            # v1.0 KIN Assignment
+            KIN[jnt_name[0]] = dict
+            if prox_bone == 'Global':
+                KIN[jnt_name[0]] = {'flx_i': flx_i, 'abd_i': abd_i, 'tw_i': tw_i,
+                                  'flx_j': flx_j, 'abd_j': abd_j, 'tw_j': tw_j}
+            elif prox_bone == 'TibiaOFM':
+                KIN[jnt_name[0]] = {'flx': alpha, 'abd': gamma, 'tw': beta}
+            else:
+                KIN[jnt_name[0]] = {'flx': alpha, 'abd': beta, 'tw': gamma}
+
+        elif version == "1.1":
+            # Version 1.1 LOGIC
+            # v1.1 calls makeax once at the top
+            floatax, _, prox_x, dist_x, prox_y, dist_y, prox_z, dist_z = makeax(pax, dax)
+
+            if prox_bone == 'Global':
+                # v1.1 Global calculations
+                flx_i = angle(dist_y, prox_x)
+                abd_i = angle(prox_y, dist_y)
+                tw_i = angle(dist_x, prox_y)
+                flx_j = angle(prox_y, dist_y)
+                abd_j = angle(dist_y, prox_x)
+                tw_j = angle(dist_x, prox_x)
+
+            # v1.1 non-Global calculations
+            if prox_bone == 'ForeFoot':
+                alpha = angle(floatax, prox_y)
+                beta = -angle(prox_z, dist_x)
+                gamma = angle(floatax, dist_z)
+            else:
+                alpha = angle(floatax, prox_x)
+                beta = - angle(prox_z, dist_x)
+                gamma = angle(floatax, dist_z)
+
+            # v1.1 KIN Assignment
+            KIN[jnt_name[0]] = dict
+            if prox_bone == 'Global':
+                KIN[jnt_name[0]] = {'flx_i': flx_i, 'abd_i': abd_i, 'tw_i': tw_i,
+                                  'flx_j': flx_j, 'abd_j': abd_j, 'tw_j': tw_j}
+            elif prox_bone == 'HindFoot':
+                KIN[jnt_name[0]] = {'flx': alpha, 'abd': beta, 'tw': gamma}
+            else:
+                KIN[jnt_name[0]] = {'flx': alpha, 'abd': gamma, 'tw': beta}
+        
         else:
-            alpha = angle(floatax, prox_x)
-            beta = - angle(prox_z, dist_x)
-            gamma = angle(floatax, dist_z)
-
-        KIN[jnt_name[0]] = dict
-        if prox_bone == 'Global':
-            KIN[jnt_name[0]] = {'flx_i': flx_i, 'abd_i': abd_i, 'tw_i': tw_i,
-                                'flx_j': flx_j, 'abd_j': abd_j, 'tw_j': tw_j}
-        elif prox_bone == 'HindFoot':
-            KIN[jnt_name[0]] = {'flx': alpha, 'abd': beta, 'tw': gamma}
-        else:
-            KIN[jnt_name[0]] = {'flx': alpha, 'abd': gamma, 'tw': beta}
+            raise ValueError(f"Unknown kinematics version: {version}")
 
     return KIN
 
@@ -277,5 +261,3 @@ def refsystem(data, KIN, version):
     data = addchannelsgs(data, KIN)
 
     return data, KIN
-
-
